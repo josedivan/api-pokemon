63fd4848e3247a6417aafca10aa5850c759c92bb
Josedivan <edivanlima23@gmail.com>
2021-07-12 11:34:10 -0300

    first commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('meu-pokemon app is running!');
+    expect(await page.getTitleText()).toEqual('fabrica-de-pokemon app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/meu-pokemon'),
+      dir: require('path').join(__dirname, './coverage/fabrica-de-pokemon'),
       subdir: '.',
       reporters: [
         { type: 'html' },
